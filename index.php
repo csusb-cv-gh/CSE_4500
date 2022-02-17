82e87028ce25c4176bbe5bbe833b97e562626c92
csusb-cv-gh <63890345+csusb-cv-gh@users.noreply.github.com>
2022-02-16 18:46:36 -0800

    Update index.php

--- index.php
+++ index.php
@@ -51,17 +51,16 @@
         <div class="vertical-center">
           <h2>Experience</h2>
           <?php foreach($json_data['experience'] AS $experience) { ?>
-              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="flex-grow-1">
-                  <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
-                  <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
-                  <p><?php echo $experience['description']; ?></p>
-                </div>
-                <div class="flex-shrink-0">
-                  <span class="text-primary"><?php echo $experience['period']; ?></span>
-                </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div class="flex-grow-1">
+                <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+                <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+                <p><?php echo $experience['description']; ?></p>
               </div>
-          <?php  } ?>
+              <div class="flex-shrink-0">
+                <span class="text-primary"><?php echo $experience['period']; ?></span>
+              </div>
+            </div>
         </div>
       </div>
       <hr>
@@ -75,17 +74,10 @@
       <div id="interests" class="content-item"> <!-- Skills -->
         <div class="vertical-center">
           <h2>Interests</h2>
-          <?php foreach($json_data['experience'] AS $experience) { ?>
-              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                <div class="flex-grow-1">
-                  <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
-                  <p><?php echo $experience['description']; ?></p>
-                </div>
-                <div class="flex-shrink-0">
-                  <span class="text-primary"><?php echo $experience['period']; ?></span>
-                </div>
-              </div>
+          <?php foreach($json_data['skills'] AS $skills) { ?>
+          <h3 class="mb-0"><?php echo $skills['skill']; ?></h3>
           <?php  } ?>
+          <p>...</p>
         </div>
       </div>
       <hr>
@@ -95,4 +87,3 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
-
